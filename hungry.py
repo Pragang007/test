46546bb49fc68d0fda4f044ca73b566e1efcb551
unknown <pragang007@gmail.com>
2022-08-23 16:10:46 +0530

    again Yo

--- hungry.py
+++ hungry.py
@@ -1,9 +1,5 @@
 hungry = input("are you hungry")
-<<<<<<< HEAD
 if hungry == "yes" and hungry == "yo":
-=======
-if hungry == "yes" and hungry == "Y":
->>>>>>> c7618a5fdda4a905b798d217cd20778494e49adb
     print("Eat Samosa")
     print("Eat Pizza")
     print("Eat Burger")
